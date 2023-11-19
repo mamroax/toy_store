92fa78a6a7298b513bc8ee2ee64e7526d30bcdf1
mamroax <mamroax@gmail.com>
2023-11-19 11:38:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    // дописываю код, пока не успел доделать
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/Toy.java
@@ -0,0 +1,12 @@
+/**
+ * id игрушки,
+ текстовое название,
+ количество
+ частота выпадения игрушки (вес в % от 100)
+ */
+public class Toy {
+    private int id;
+    private String name;
+    private int quantity; // количество игрушек
+    private int rate; // частота выпадения игрушки
+}
